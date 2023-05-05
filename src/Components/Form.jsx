586694a7806c8d6b2b10156e943da6df645b87f8
Bogdan Rossov <BogdanRossov@gmail.com>
2023-05-05 19:39:33 +0300

    change validation logic

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -9,8 +9,6 @@ function ReservationForm() {
   const [comment, setComment] = useState("");
   const [isValid, setIsValid] = useState(true);
 
-  
-
   function handleTowerChange(event) {
     setTower(event.target.value);
   }
@@ -53,7 +51,6 @@ function ReservationForm() {
     return selectedDate > dateNow;
   }
 
-  
   function handleSubmit(event) {
     event.preventDefault();
     if (isValidDate()) {
@@ -66,6 +63,7 @@ function ReservationForm() {
         comment,
       };
       console.log(JSON.stringify(data));
+      setIsValid(true);
       handleReset();
     } else {
       setIsValid(false);
@@ -84,91 +82,84 @@ function ReservationForm() {
 
   return (
     <div className="form-control">
-      {isValid ? (
-        <form className="form" onSubmit={handleSubmit} onReset={handleReset}>
-          <label className="form-label">
-            Choose a tower:
-            <select
-              className="form-select"
-              value={tower}
-              onChange={handleTowerChange}
-            >
-              <option value="A">Tower A</option>
-              <option value="B">Tower B</option>
-            </select>
-          </label>
+      <form className="form" onSubmit={handleSubmit} onReset={handleReset}>
+        <label className="form-label">
+          Choose a tower:
+          <select
+            className="form-select"
+            value={tower}
+            onChange={handleTowerChange}
+          >
+            <option value="A">Tower A</option>
+            <option value="B">Tower B</option>
+          </select>
+        </label>
 
-          <label className="form-label">
-            Choose a floor:
-            <select
-              className="form-select"
-              value={floor}
-              onChange={handleFloorChange}
-            >
-              {Array.from({ length: 25 }, (_, i) => i + 3).map((f) => (
-                <option key={f} value={f}>
-                  Floor {f}
-                </option>
-              ))}
-            </select>
-          </label>
+        <label className="form-label">
+          Choose a floor:
+          <select
+            className="form-select"
+            value={floor}
+            onChange={handleFloorChange}
+          >
+            {Array.from({ length: 25 }, (_, i) => i + 3).map((f) => (
+              <option key={f} value={f}>
+                Floor {f}
+              </option>
+            ))}
+          </select>
+        </label>
 
-          <label className="form-label">
-            Choose a room:
-            <select
-              className="form-select"
-              value={room}
-              onChange={handleRoomChange}
-            >
-              {Array.from({ length: 10 }, (_, i) => i + 1).map((r) => (
-                <option key={r} value={r}>
-                  Room {r}
-                </option>
-              ))}
-            </select>
-          </label>
-          <label className="form-label">
-            Choose a date:
-            <input
-              className="form-select"
-              type="date"
-              value={date.toISOString().substr(0, 10)}
-              onChange={handleDateChange}
-            />
-          </label>
+        <label className="form-label">
+          Choose a room:
+          <select
+            className="form-select"
+            value={room}
+            onChange={handleRoomChange}
+          >
+            {Array.from({ length: 10 }, (_, i) => i + 1).map((r) => (
+              <option key={r} value={r}>
+                Room {r}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label className="form-label">
+          Choose a date:
+          <input
+            className="form-select"
+            type="date"
+            value={date.toISOString().substr(0, 10)}
+            onChange={handleDateChange}
+          />
+        </label>
 
-          <label className="form-label">
-            Choose a time:
-            <input
-              className="form-select"
-              type="time"
-              value={time}
-              onChange={handleTimeChange}
-            />
-          </label>
+        <label className="form-label">
+          Choose a time:
+          <input
+            className={isValid ? "form-select" : "invalid-select"}
+            type="time"
+            value={time}
+            onChange={handleTimeChange}
+          />
+        </label>
 
-          <label className="form-label">
-            Comment:
-            <textarea
-              className="form-textarea"
-              value={comment}
-              onChange={handleCommentChange}
-            />
-          </label>
+        <label className="form-label">
+          Comment:
+          <textarea
+            className="form-textarea"
+            value={comment}
+            onChange={handleCommentChange}
+          />
+        </label>
 
-          <button className="form-button" type="submit">
-            Submit
-          </button>
-          <button className="form-button" type="reset">
-            Reset
-          </button>
-        </form>
-      ) : (
-        <div className="form-error">
-          <p>Введите корректную дату и время</p>
-          <button className="form-button" onClick={handleReset}>Понятно</button>
-        </div>
-      )}
+        <button className="form-button" type="submit">
+          Submit
+        </button>
+        <button className="form-button" type="reset">
+          Reset
+        </button>
+      </form>
     </div>
   );
 }
